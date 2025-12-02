ba724fb76594be0615d2c6f5fe89de8bf63c259e
Msol9 <ms.bksol@gmail.com>
2025-12-02 10:23:33 -0600

    Pedir datos para el modelo m/m/c

--- main.java
+++ main.java
@@ -7,26 +7,26 @@ public class Main{
     public static void main(String []args){
         Scanner lector=new Scanner(System.in);
         byte opcion=0;
-        double μ, λ, p, Lq, Wq, W;
+        double μ, λ, p, Lq, Wq, W, c, a, p0, pEspera, L;
         do{
             System.out.println ("\nMENU DE LINEAS DE ESPERA");
-            System.out.println("1. Modelo m/m/1");
-            System.out.println("2. Modelo m/m/c");
-            System.out.println("3. Modelo m/m/1/k")
+            System.out.println("1. Modelo M/M/1");
+            System.out.println("2. Modelo M/M/c");
+            System.out.println("3. Modelo M/M/1/k")
             System.out.println("4. Salir");
             opcion=lector.nextByte();
 
             switch(opcion){
                 case 1 ->{
-                System.out.println("Resolucion de politica con el modelo m/m/1");
-                System.out.println("Ingresa es numero promedio de llegadas (λ) ");
+                System.out.println("\nResolucion de politica con el modelo M/M/1");
+                System.out.println("\nIngresa el numero promedio de llegadas (λ): ");
                 λ=lector.nextDouble();
                 System.out.println("Ingresa el numero promedio de servicio (μ): ");
                 μ=lector.nextDouble();
                 
-                System.out.println("Los resultados de la politica son:");
+                System.out.println("\nLos resultados de la politica son:");
                 p= λ/μ;
-                System.out.println("Utilizacion del sistema: " + p*100 + "% de utilizacion");
+                System.out.println("\nUtilizacion del sistema: " + p*100 + "% de utilizacion");
                 Lq=Math.pow(λ, 2)/(μ*(μ-λ));
                 System.out.println("Numero promedio de clientes en la cola: " + Lq + "clientes");
                 Wq=Lq/λ;
@@ -35,7 +35,16 @@ public class Main{
                 System.out.println("Tiempo total en el sistema: " + W + "horas o "+ W*60 + "minutos" );
                 }
                 case 2 ->{
+                System.out.println("\n Resolucion de politica con el modelo m/m/c");
+                System.out.println("\n Ingresa el numero promedio de llegadas (λ): ");
+                λ=lector.nextDouble();
+                System.out.println("Ingresa el numero promedio de servicio (μ): ");
+                μ=lector.nextDouble();
+                System.out.println("Ingresa el numero de servidores (c): ");
+                c=lector.nextDouble();
 
+                System.out.println("\nLos resultados de la politica son: ");
+                
                 }
                 case 3->{
 
